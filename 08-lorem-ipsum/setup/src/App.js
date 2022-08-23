64eac90858cc1d62e218dd2c1ffc2786692fc2be
Boldmaa <s.boldmaa@gmail.com>
2022-08-23 19:09:42 +0800

    lorem ipsum implementation

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import data from './data';
+
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setText(data);
+    }
+
+    return (
+        <section className={'section-center'}>
+          <h3>tired of boring lorem ipsum?</h3>
+          <form className={'lorem-form'} onSubmit={handleSubmit}>
+              <label htmlFor={'amount'}>
+                  paragraphs:
+              </label>
+              <input type={'number'} name={'amount'} id={'amount'} value={count} onChange={(e)=>setCount(e.target.value)}/>
+              <button type={'submit'} className={'btn'}>generate</button>
+          </form>
+            <article className={'lorem-text'}>
+                {text.map((item, index) => {
+                    return <p key={index}>{item}</p>
+                })}
+            </article>
+        </section>
     )
 }
 
